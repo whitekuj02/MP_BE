75fb1f94ddb2842918151bbe432bd669ddb5d0f4
kimuijin <whitekuj02@naver.com>
2023-04-03 20:39:21 +0900

    spring 환경설정

---
+++ src/main/java/com/example/mp_be/MpBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mp_be/MpBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
